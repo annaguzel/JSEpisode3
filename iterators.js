ddcd43b2acab5e9ea3730eecbde3bd450bf44f73
Annaguzel <you@example.com>
2020-03-05 16:13:58 +0200

    Episode 3

--- iterators.js
+++ iterators.js
@@ -5,7 +5,7 @@
  * - Logs every element of the array
  ************************************/
 const logger = function(array) {
-  // Your code here
+  array.forEach(element => console.log(element));
 };
 
 /*************************************
@@ -20,19 +20,19 @@ const logger = function(array) {
  *   C = (F - 32) * (5/9)
  ************************************/
 const toCelsius = function(temperatures) {
-  // Your code here
+ return temperatures.map(temperature => (temperature - 32)*(5/9));
 };
 
 /**************************************
  * hottestDays(temperatures, threshhold)
- *
  * - Accepts an array of temperatures
  * - Accepts a threshhold temperature
  * - Returns an array of temperatures
  *   that exceed the threshhold
  ***************************************/
 const hottestDays = function(temperatures, threshhold) {
-  // Your code here
+  return temperatures.filter(temperature => temperature >threshhold);
+
 };
 
 /******************************************
@@ -50,7 +50,9 @@ const hottestDays = function(temperatures, threshhold) {
  *       all previous functions
  *******************************************/
 const logHottestDays = function(temperatures, threshhold) {
-  // Your code here
+  let array= hottestDays(temperatures, threshhold);
+  let celsius=toCelsius(array);
+  logger(celsius);
 };
 
 export { logger, toCelsius, hottestDays, logHottestDays };
